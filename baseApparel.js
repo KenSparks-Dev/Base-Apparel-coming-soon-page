4067f3d3a133ba21994d8eca6e0ae8457d510314
Ken Sparks <kensparks.dev@gmail.com>
2020-05-13 23:40:09 -0400

    background changes, email submit button javascript

--- baseApparel.js
+++ baseApparel.js
@@ -10,3 +10,25 @@ function validateEmail(InputText) {
     return false;
   }
 }
+let submitBtn = document.querySelector(".btn")
+submitBtn.style.marginLeft = "-75px"
+
+let errorBtn = document.querySelector(".error-btn")
+errorBtn.style.marginLeft = "-45px"
+errorBtn.style.display = "none"
+
+let emailForm = document.querySelector(".email")
+ 
+function errorResponse(){
+    submitBtn.style.marginLeft = "-10px"
+    submitBtn.style.backgroundImage = "linear-gradient(to right ,hsl(0, 80%, 86%),hsl(0, 74%, 74%))"
+    errorBtn.style.display = "inline"
+    emailForm.style.border = "4px solid red"
+
+}
+
+submitBtn.addEventListener("click", errorResponse);
+
+function goodResponse() {
+  emailForm.style.border = "4px solid limegreen";
+}
